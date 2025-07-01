811a77db2825688fe21b2c6133aa82158c457f3b
ah-naf <sheikhahnafshifat@gmail.com>
2025-07-02 00:35:43 +0600

    add new metrices related to memory and processes

--- internals/metrics/system_metrics.go
+++ internals/metrics/system_metrics.go
@@ -24,6 +24,46 @@ var (
 		Name:      "cpu_usage_percent",
 		Help:      "Current CPU utilization percentage of the process",
 	})
+
+	// UNIX timestamp of when the process started
+	ProcessStartTimeSeconds = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "myapp",
+		Subsystem: "process",
+		Name:      "start_time_seconds",
+		Help:      "UNIX timestamp of when the process started",
+	})
+
+	// Number of open file descriptors
+	ProcessOpenFDs = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "myapp",
+		Subsystem: "process",
+		Name:      "open_fds",
+		Help:      "Number of open file descriptors by the process",
+	})
+
+	// Number of OS threads in this process
+	ProcessNumThreads = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "myapp",
+		Subsystem: "process",
+		Name:      "threads",
+		Help:      "Number of OS threads in the process",
+	})
+
+	// Resident (physical) memory in bytes currently used by the process
+	ProcessResidentMemoryBytes = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "myapp",
+		Subsystem: "process",
+		Name:      "resident_memory_bytes",
+		Help:      "Resident (physical) memory in bytes currently used by the process",
+	})
+
+	// Virtual memory in bytes allocated by the process (RSS + swap + mappings)
+	ProcessVirtualMemoryBytes = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "myapp",
+		Subsystem: "process",
+		Name:      "virtual_memory_bytes",
+		Help:      "Virtual memory in bytes allocated by the process (RSS + swap + mappings)",
+	})
 )
 
 func RunCPUMetrics(interval time.Duration) {
@@ -32,11 +72,15 @@ func RunCPUMetrics(interval time.Duration) {
 		panic("metrics: cannot create process handle: " + err.Error())
 	}
 
+	if ms, err := proc.CreateTime(); err == nil {
+		ProcessStartTimeSeconds.Set(float64(ms) / 1000.0)
+	}
+
 	var lastTotal float64
 
 	go func() {
 		for {
-			// 3a) CPU seconds: user + system
+			// 1) CPU seconds: user + system
 			if times, err := proc.Times(); err == nil {
 				total := times.User + times.System
 				delta := total - lastTotal
@@ -47,9 +91,29 @@ func RunCPUMetrics(interval time.Duration) {
 				lastTotal = total
 			}
 
-			// 3b) CPU usage % over the last interval
+			// 2) CPU usage % over the last interval
 			if pct, err := proc.Percent(0); err == nil {
 				ProcessCPUUsagePercent.Set(pct)
+			}
+
+			// 3) Open FDs
+			if fds, err := proc.NumFDs(); err == nil {
+				ProcessOpenFDs.Set(float64(fds))
+			}
+
+			// 4) Thread count
+			if tn, err := proc.NumThreads(); err == nil {
+				ProcessNumThreads.Set(float64(tn))
+			}
+
+			// Resident memory
+			if mi, err := proc.MemoryInfo(); err == nil {
+				ProcessResidentMemoryBytes.Set(float64(mi.RSS))
+			}
+
+			// Virtual memory
+			if mi, err := proc.MemoryInfo(); err == nil {
+				ProcessVirtualMemoryBytes.Set(float64(mi.VMS))
 			}
 
 			time.Sleep(interval)
